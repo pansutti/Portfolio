4e77d98edfaf52403143d2fa0fbe4677a72ee3b5
Pansutti <pansuttitorres@hotmail.com>
2024-01-16 15:53:14 -0300

    commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ function NavBar() {
       <a className="text-4xl font-bold" href="/">Pansutti.dev</a>
       <CiMenuBurger className="text-6xl cursor-pointer hidden"/>
       <nav className="text-2xl font-semibold">
-        <a className="ml-10 transition-colors duration-300 text-blue-500" href="/home">Home</a>
+        <a className="ml-10 transition-colors duration-300 hover:text-blue-500" href="/home">Home</a>
         <a className="ml-10 transition-colors duration-300 hover:text-blue-500" href="/about">Sobre</a>
         <a className="ml-10 transition-colors duration-300 hover:text-blue-500" href="/education">Formação</a>
         <a className="ml-10 transition-colors duration-300 hover:text-blue-500" href="/skills">Habilidades</a>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import NavBar from "../components/NavBar";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 function App() {
   return (
     <>
       <NavBar />
-      <section className="flex items-center min-h-full p-[9%] pt-[18%]">
+      <section className="flex items-center min-h-full p-[9%] pt-[12%]">
         <div className="max-w-[60rem]">
           <h1 className="text-[5.6rem] font-bold leading-[1.3]">
             Oi, eu sou <span>o Pansutti</span>
@@ -37,6 +38,28 @@ function App() {
               <span className="relative z-10">Meu CV</span>
             </a>
           </div>
+        </div>
+        <div className="absolute top-[51rem] w-[100px] flex justify-between">
+          <a
+            className="relative inline-flex justify-center items-center w-[40px] h-[40px] bg-transparent border-solid rounded-[50%] text-[20px] z-1 overflow-hidden group"
+            href="https://www.linkedin.com/in/luiz-gustavo-pansutti-torres/"
+            target="_blank" rel="noreferrer"
+          >
+            <span className="absolute inset-0 bg-blue-500 transition-all transform scale-x-0 origin-left group-hover:scale-x-100"></span>
+            <i className="relative z-10">
+              <FaLinkedinIn />
+            </i>
+          </a>
+          <a
+            className="relative inline-flex justify-center items-center w-[40px] h-[40px] bg-transparent border-solid rounded-[50%] text-[20px] z-1 overflow-hidden group"
+            href="https://github.com/pansutti"
+            target="_blank" rel="noreferrer"
+          >
+            <span className="absolute inset-0 bg-blue-500 transition-all transform scale-x-0 origin-left group-hover:scale-x-100"></span>
+            <i className="relative z-10">
+              <FaGithub />
+            </i>
+          </a>
         </div>
       </section>
     </>
